f419b879c0305afd21a5e60cf1fec8573f464068
Emanuel Bispo <emanuelbispoc@gmail.com>
2024-02-23 19:15:37 -0300

    Commit #2: salvar produto no banco de dados

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,22 +12,30 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
 public class ProdutosDAO {
     
-    Connection conn;
+    Connection conn = new conectaDAO().connectDB();
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+    public void cadastrarProduto (ProdutosDTO produto){     
+        try{
+            prep = conn.prepareStatement("INSERT INTO produtos (nome, valor, status) values (?,?,?)");
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3,produto.getStatus());
+            
+            prep.executeUpdate();
+            
+            conn.close();
+        }catch(SQLException e) {
+            System.out.println("AAA"+e.getMessage());
+        }    
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
